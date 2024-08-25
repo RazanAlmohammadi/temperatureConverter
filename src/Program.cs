e7e7475b54aa5b028ae3023a1be3e49c71ff590d
razan4577 <razanalmohammadi@icloud>
2024-08-25 15:13:27 +0300

    Basic conversion

--- src/Program.cs
+++ src/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+class Program{
+     static string TempConvert(int temperature,string unit){
+    if(unit=="F"){
+       return ((temperature-32)*5/9+" C ");
+        
+    }
+    else{
+         return ((temperature*1.8)+32 +" F ");
+    }
+   
+}
+
+    public static void Main(string[] args) {
+        Console.WriteLine(TempConvert(32,"F"));
+         Console.WriteLine(TempConvert(100,"C"));
+        
+    }
+}
