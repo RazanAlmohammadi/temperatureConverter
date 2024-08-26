ae5d7f69224e6e8518abef6eb911143580cb74fd
razan4577 <razanalmohammadi@icloud>
2024-08-26 12:58:39 +0300

    format the code for level 1

--- src/Program.cs
+++ src/Program.cs
@@ -1,17 +1,22 @@
 ﻿
-class Program{
-     static string TempConvert(int temperature,string unit){
-    if(unit=="F"){
-       return ((temperature-32)*5/9+" C ");
+class Program
+{
+     static string TempConvert(int temperature,string unit)
+     {
+       if(unit=="F")
+    {
+           return (temperature-32)*5/9+" C ";
         
     }
-    else{
-         return ((temperature*1.8)+32 +" F ");
+       else
+       {
+           return (temperature*1.8)+32 +" F ";
     }
    
 }
 
-    public static void Main(string[] args) {
+    public static void Main(string[] args)
+     {
         Console.WriteLine(TempConvert(32,"F"));
          Console.WriteLine(TempConvert(100,"C"));
         
